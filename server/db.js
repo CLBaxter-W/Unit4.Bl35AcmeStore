21d3c436d47998d1abdf55fd2c5ce27354621b36
CLBaxter-W <168162275+CLBaxter-W@users.noreply.github.com>
2024-07-08 14:27:16 -0600

    updates to work

--- server/db.js
+++ server/db.js
@@ -1,4 +1,120 @@
 const pg = require("pg");
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
+
 const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/the_acme_store"
+  process.env.DATABASE_URL || "postgres://postgres:@localhost/the_acme_store"
 );
+
+const createTables = async () => {
+  let SQL = `
+    
+            DROP TABLE IF EXISTS users CASCADE;
+            DROP TABLE IF EXISTS products CASCADE;
+            DROP TABLE IF EXISTS favorites CASCADE;
+    
+            CREATE TABLE IF NOT EXISTS users(
+                id UUID PRIMARY KEY,
+                username VARCHAR(64) NOT NULL UNIQUE,
+                password VARCHAR(128) NOT NULL UNIQUE
+            );
+            CREATE TABLE IF NOT EXISTS products(
+                id UUID PRIMARY KEY,
+                name VARCHAR(64) NOT NULL UNIQUE
+            );
+            CREATE TABLE IF NOT EXISTS favorites(
+                id UUID PRIMARY KEY,
+                user_id UUID REFERENCES users(id) NOT NULL,
+                product_id UUID REFERENCES products(id) NOT NULL,
+                CONSTRAINT unique_user_id_product_id UNIQUE (user_id, product_id)
+            );
+        `;
+
+  await client.query(SQL);
+};
+
+const createUser = async ({ username, password }) => {
+  const hashPassword = await bcrypt.hash(password, 5);
+
+  const SQL = `
+          INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+        `;
+  const response = await client.query(SQL, [uuid.v4(), username, hashPassword]);
+  return response.rows[0];
+};
+
+const createProduct = async ({ name }) => {
+  const SQL = `
+          INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
+        `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const createUserProduct = async ({ user_id, product_id }) => {
+  const SQL = `
+          INSERT INTO favorites(id, user_id, product_id) 
+          VALUES($1, $2, $3) 
+          RETURNING *
+        `;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  return response.rows[0];
+};
+
+const fetchUsers = async () => {
+  const SQL = `
+      SELECT *
+      FROM users
+        `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchProducts = async () => {
+  const SQL = `
+      SELECT *
+      FROM products
+        `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchUserProducts = async ({ id }) => {
+  const SQL = `
+         SELECT users.username as user_name, 
+           products.name as product_name,
+           favorites.id as user_product_id,
+           favorites.user_id as user_id,
+           favorites.product_id as product_id
+         FROM favorites
+         INNER JOIN 
+           users on users.id = favorites.user_id
+         INNER JOIN
+           products on products.id = favorites.product_id   
+         WHERE 
+           favorites.user_id = $1;`;
+
+  const response = await client.query(SQL, [id]);
+
+  return response.rows[0];
+};
+
+const destroyUserProduct = async ({ user_product_id, user_id }) => {
+  const SQL = `
+            DELETE FROM favorites
+            WHERE id = $1 AND user_id=$2
+        `;
+  await client.query(SQL, [user_product_id, user_id]);
+};
+
+module.exports = {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  createUserProduct,
+  fetchUsers,
+  fetchProducts,
+  fetchUserProducts,
+  destroyUserProduct,
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
+const {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  createUserProduct,
+  fetchUsers,
+  fetchProducts,
+  fetchUserProducts,
+  destroyUserProduct,
+} = require("./db");
+
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -5,4 +17,136 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`I am listening on port number ${PORT}`);
+});
+
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+
+  const response = await createTables();
+  console.log("Created Tables");
+
+  const [Max, Gryphon, Kalu, Hero, CT, Bn, OC, Br] = await Promise.all([
+    createUser({ username: "Max", password: "oldDog" }),
+    createUser({ username: "Gryphon", password: "youngDog" }),
+    createUser({ username: "Kalu", password: "goodDog" }),
+    createUser({ username: "Hero", password: "fastDog" }),
+
+    createProduct({ name: "chew toy" }),
+    createProduct({ name: "Bone" }),
+    createProduct({ name: "Obstacle Course" }),
+    createProduct({ name: "Brush" }),
+  ]);
+
+  console.log(await fetchUsers());
+  console.log(await fetchProducts());
+
+  const [
+    products1,
+    products2,
+    products3,
+    products4,
+    products5,
+    products6,
+    products7,
+  ] = await Promise.all([
+    createUserProduct({
+      user_id: Max.id,
+      product_id: CT.id,
+    }),
+    createUserProduct({
+      user_id: Gryphon.id,
+      product_id: Bn.id,
+    }),
+    createUserProduct({
+      user_id: Gryphon.id,
+      product_id: OC.id,
+    }),
+    createUserProduct({
+      user_id: Gryphon.id,
+      product_id: Br.id,
+    }),
+    createUserProduct({
+      user_id: Kalu.id,
+      product_id: Br.id,
+    }),
+    createUserProduct({
+      user_id: Kalu.id,
+      product_id: OC.id,
+    }),
+    createUserProduct({
+      user_id: Kalu.id,
+      product_id: Bn.id,
+    }),
+  ]);
+
+  console.log("Sending just named parameter");
+  console.log(await fetchUserProducts({ id: Max.id }));
+  console.log("Testing sending Max");
+  console.log(await fetchUserProducts(Max));
+
+  console.log(await fetchUserProducts({ id: Gryphon.id }));
+  console.log(await fetchUserProducts({ id: Kalu.id }));
+
+  await destroyUserProduct({
+    user_product_id: products7.id,
+    user_id: products7.user_id,
+  });
+};
+
+init();
+
+// Express API
+
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/products", async (req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/users/:id/userproducts", async (req, res, next) => {
+  try {
+    res.send(await fetchUserProducts({ id: req.params.id }));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post("/api/users/:user_id/userproducts/:id", async (req, res, next) => {
+  try {
+    res.status(201).send(
+      await createUserProduct({
+        user_id: req.params.user_id,
+        product_id: req.params.id,
+      })
+    );
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete("/api/users/:user_id/userProducts/:id", async (req, res, next) => {
+  try {
+    await destroyUserProduct({
+      user_id: req.params.user_id,
+      user_product_id: req.params.id,
+    });
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({ error: err.message || err });
 });
